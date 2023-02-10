ae28aea99a974b1248b074d0534d24682359346e
sneha-088 <117896781+sneha-088@users.noreply.github.com>
2023-02-10 17:54:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+let textBox = document.getElementById("evaluatedText");
+let wordCountDisplay = document.getElementById("wordCount");
+
+textBox.addEventListener("input", function() {
+let wordCount = countWords(textBox.value);
+wordCountDisplay.innerHTML = wordCount;
+});
+
+function countWords(text) {
+return text.split(" ").length;
+}
